2347f447d1883c38ad58e84b277681556f0f018c
kFurusho <d.guitar.c@gmail.com>
2019-10-03 17:37:57 +0900

    Use large size texture

--- src/foundation/core/Config.ts
+++ src/foundation/core/Config.ts
@@ -6,8 +6,10 @@ let maxSkeletonNumber = 16;
 let maxCameraNumber = 15;
 let maxSizeLimitOfNonCompressedTexture = 512;
 let maxSkeletalBoneNumber = 250;
-let dataTextureWidth = Math.pow(2, 11);
-let dataTextureHeight = Math.pow(2, 11);
+let dataTextureWidth = Math.pow(2, 12);
+let dataTextureHeight = Math.pow(2, 12);
 
-export default { maxEntityNumber, maxLightNumberInShader, maxVertexMorphNumberInShader, maxMaterialInstanceForEachType,
-  maxSkeletonNumber, maxCameraNumber, maxSizeLimitOfNonCompressedTexture, maxSkeletalBoneNumber, dataTextureWidth, dataTextureHeight };
+export default {
+  maxEntityNumber, maxLightNumberInShader, maxVertexMorphNumberInShader, maxMaterialInstanceForEachType,
+  maxSkeletonNumber, maxCameraNumber, maxSizeLimitOfNonCompressedTexture, maxSkeletalBoneNumber, dataTextureWidth, dataTextureHeight
+};
