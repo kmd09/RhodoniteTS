94354ed26735eb378f9b15e01a7ebd9150bdda06
emadurandal <emadurandal@gmail.com>
2019-12-06 05:10:38 +0900

    Add the fetchVec4 GLSL function

--- src/webgl/shaders/GLSLShader.ts
+++ src/webgl/shaders/GLSLShader.ts
@@ -353,6 +353,47 @@ uniform sampler2D u_dataTexture;
   //   return ${this.glsl_texture}( tex, arg * (index + 0.5) );
   // }
 
+highp vec4 fetchVec4(highp sampler2D tex, highp float componentIdx, highp vec2 invSize){
+  float modN = mod(componentIdx, 4.0);
+  float index = 4.0 * floor(componentIdx / 4.0) / 4.0;
+  if (modN == 0.0) {
+    highp float t = (index + 0.5) * invSize.x;
+    highp float x = fract(t);
+    highp float y = (floor(t) + 0.5) * invSize.y;
+    return ${this.glsl_texture}( tex, vec2(x, y) );
+  } else if (modN == 1.0) {
+    highp float t0 = (index + 0.5) * invSize.x;
+    highp float x0 = fract(t0);
+    highp float y0 = (floor(t0) + 0.5) * invSize.y;
+    vec4 texel0 = ${this.glsl_texture}( tex, vec2(x0, y0) );
+    highp float t1 = (index + 1.0 + 0.5) * invSize.x;
+    highp float x1 = fract(t1);
+    highp float y1 = (floor(t1) + 0.5) * invSize.y;
+    vec4 texel1 = ${this.glsl_texture}( tex, vec2(x1, y1) );
+    return vec4(texel0.yzw, texel1.x);
+  } else if (modN == 2.0) {
+    highp float t0 = (index + 0.5) * invSize.x;
+    highp float x0 = fract(t0);
+    highp float y0 = (floor(t0) + 0.5) * invSize.y;
+    vec4 texel0 = ${this.glsl_texture}( tex, vec2(x0, y0) );
+    highp float t1 = (index + 1.0 + 0.5) * invSize.x;
+    highp float x1 = fract(t1);
+    highp float y1 = (floor(t1) + 0.5) * invSize.y;
+    vec4 texel1 = ${this.glsl_texture}( tex, vec2(x1, y1) );
+    return vec4(texel0.zw, texel1.xy);
+  } else {
+    highp float t0 = (index + 0.5) * invSize.x;
+    highp float x0 = fract(t0);
+    highp float y0 = (floor(t0) + 0.5) * invSize.y;
+    vec4 texel0 = ${this.glsl_texture}( tex, vec2(x0, y0) );
+    highp float t1 = (index + 1.0 + 0.5) * invSize.x;
+    highp float x1 = fract(t1);
+    highp float y1 = (floor(t1) + 0.5) * invSize.y;
+    vec4 texel1 = ${this.glsl_texture}( tex, vec2(x1, y1) );
+    return vec4(texel0.w, texel1.xyz);
+  }
+}
+
 highp vec4 fetchElement(highp sampler2D tex, highp float index, highp vec2 invSize){
   highp float t = (index + 0.5) * invSize.x;
   highp float x = fract(t);
