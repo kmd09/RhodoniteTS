be150be3491d09233e6222a2c4c789449085920f
emadurandal <emadurandal@gmail.com>
2019-12-12 22:41:47 +0900

    chore: Implementing WebXRSystem

--- src/xr/WebXRSystem.ts
+++ src/xr/WebXRSystem.ts
@@ -1,5 +1,15 @@
+
+declare const XRSession: any;
+
+declare global {
+  interface Navigator {
+    xr: any;
+  }
+}
+
 export default class WebXRSystem {
   private static __instance: WebXRSystem;
+  private __device: any;
 
   private constructor() {
   }
@@ -11,5 +21,18 @@ export default class WebXRSystem {
 
     return this.__instance;
   }
-}
 
+  async readyForWebXR(requestButtonDom: HTMLElement) {
+    if (navigator.xr && XRSession.prototype.requestHitTest) {
+      try {
+        this.__device = await navigator.xr.requestDevice();
+      } catch (e) {
+        console.error('No WebXR devices found.');
+        return;
+      }
+    } else {
+      console.error('WebXR is not supported in this environment.');
+      return;
+    }
+  }
+}
